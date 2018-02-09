21b4a8dfcda9315d276c805002e66731aef437ae
Valentina Abramova <vabramova1@gmail.com>
2018-02-09 10:04:12 -0600

    forgot to push

--- app/app.js
+++ app/app.js
@@ -1,10 +1,33 @@
-angular.module('devmtnTravel', ['ui.router']).config( function ( $stateProvider, $urlRouterProvider ) {
-  $stateProvider
-    .state('home',{
-      url:'/',
-      templateUrl: "app/about/aboutTmpl.html"
-    });
+angular
+  .module("devmtnTravel", ["ui.router"])
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("home", {
+        url: "/",
+        templateUrl: "app/about/aboutTmpl.html"
+      })
+      .state("packages", {
+        url: "/packages/:country",
+        templateUrl: "app/packages/packagesTmpl.html"
+      })
+      .state("locations", {
+        url: "/locations",
+        templateUrl: "app/locations/locationsTmpl.html"
+      })
+      .state("booked", {
+        url: "/booked/:id",
+        templateUrl: "app/booked/bookedTmpl.html"
+      })
+      .state("adventurers", {
+        url: "/adventurers",
+        templateUrl: "app/about/adventurers/adventurersTmpl.html",
+        parent: "home"
+      })
+      .state("contact", {
+        url: "/contact",
+        templateUrl: "app/about/contact/contactTmpl.html",
+        parent: "home"
+      });
 
-  $urlRouterProvider
-    .otherwise('/');
-});
+    $urlRouterProvider.otherwise("/");
+  });
---
+++ app/locations/locationsCtrl.js
@@ -0,0 +1,7 @@
+angular
+  .module("devmtnTravel")
+  .controller("locationsCtrl", function($scope, mainSrvc) {
+    mainSrvc.getTravelInfo().then(function(response) {
+      $scope.locations = response.data;
+    });
+  });
